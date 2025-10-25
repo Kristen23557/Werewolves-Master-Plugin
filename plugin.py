1a1b8bffd783d1c35f37c6ea2475b5c9b955afc8
Kristen23557 <speedmod@163.com>
2025-10-25 14:21:54 +0800

    modified:   plugin.py

--- plugin.py
+++ plugin.py
@@ -61,7 +61,7 @@ ROLES = {
         "is_sub": False,
         "night_action": False,
         "day_action": False,
-        "command": None
+        "command": None,
         "description": "普通村民，没有特殊能力，通过推理找出狼人"
     },
     "seer": {
